360bc4bab11637744e6a5698d2e03facb33fc023
taketo1024 <taketo1024@gmail.com>
2023-09-05 15:23:47 +0900

    Basic/Lecture4

--- Tutorial/Basic/Lecture4.lean
+++ Tutorial/Basic/Lecture4.lean
@@ -23,7 +23,9 @@ example : Injective f ↔ Function.Injective f := Iff.rfl
 example : Injective (fun x : ℕ ↦ x + 1) := by
   -- ヒント: `rw [Injective]`をすると単射の定義に戻れる
   -- 積極的に`simp`や`apply?`等のチートコマンドを使おう！
-  sorry
+  rw [Injective]
+  intro x₁ x₂ h
+  exact Iff.mp Nat.succ_inj' h
 
 /-
 2つの単射の合成は単射。
@@ -40,7 +42,7 @@ theorem Injective.comp {f : X → Y} {g : Y → Z} (hfinj : Injective f) (hginj 
   -- 次のように`have`を使おう。
   have hf := hginj hgf
   -- すると `hf: f x₁ = f x₂`が使える。下の補足も参照。
-  sorry
+  exact hfinj hf
 
 /-
 *補足*
@@ -63,7 +65,8 @@ example {f : X → Y} {g : Y → Z} (hfinj : Injective f) (hginj : Injective g) 
   rw [Injective]
   intro x₁ x₂ hgf
   apply hfinj -- なぜ`apply`でこう書き換わるか考えよう
-  sorry
+  apply hginj
+  exact hgf
 
 -- 合成して単射なら先の写像は単射
 theorem Injective.of_comp {f : X → Y} {g : Y → Z} (hgfinj : Injective (g ∘ f)) : Injective f := by
@@ -73,8 +76,9 @@ theorem Injective.of_comp {f : X → Y} {g : Y → Z} (hgfinj : Injective (g ∘
   -- 以下のように、`have name : 示したいこと := by`と書ける。
   -- その後のインデントに注意。
   have h : g (f x₁) = g (f x₂) := by
-    sorry
-  sorry
+    exact congrArg g hf
+  apply hgfinj
+  apply h
 
 -- 強いチートコマンドを使った別解
 example {f : X → Y} {g : Y → Z} (hgfinj : Injective (g ∘ f)) : Injective f := by
@@ -110,7 +114,10 @@ example : Surjective f ↔ Function.Surjective f := Iff.rfl
 example : Surjective (fun x : ℤ ↦ x + 1) := by
   -- `apply?`等のチートコマンドを使ってもいいし、
   -- Lecture 3で学んだ`exists`を使ってもいい
-  sorry
+  rw [Surjective]
+  intro y
+  exists y - 1
+  ring 
 
 -- 以下`f`は`X`から`Y`への写像、`g`は`Y`から`Z`の写像とする。
 variable {f : X → Y} {g : Y → Z}
@@ -123,7 +130,11 @@ theorem Surjective.comp (hfsurj : Surjective f) (hgsurj : Surjective g) : Surjec
   -- これはLecture3で見たように、次で取れる。
   -- 下の補足も参照。
   have ⟨y, hy⟩ := hgsurj z
-  sorry
+  have ⟨x, hx⟩ := hfsurj y
+  exists x
+  change g (f x) = z
+  rw [hx]
+  exact hy
 
 /-
 *補足*
@@ -135,8 +146,12 @@ theorem Surjective.comp (hfsurj : Surjective f) (hgsurj : Surjective g) : Surjec
 
 -- 合成して全射なら後ろの写像は全射
 theorem Surjective.of_comp (h : Surjective (g ∘ f)) : Surjective g := by
-  sorry
-
+  rw [Surjective]
+  intro z
+  have ⟨x, hx⟩ := h z
+  use f x
+  apply hx
+  
 end Tutorial
 
 /-  
